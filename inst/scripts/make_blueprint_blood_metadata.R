84c4350b894279b2e56cf056e6edbd41aa0a11f8
Peter Hickey <peter.hickey@gmail.com>
2020-08-17 21:58:43 +1000

    Add BLUEPRINT blood (closes #2)

---
+++ inst/scripts/make_blueprint_blood_metadata.R
@@ -0,0 +1,34 @@
+# TODO
+write.csv(
+  file = "../../extdata/metadata_blueprint_blood.csv",
+  data.frame(
+    # Title = sprintf("Wu %s mouse kidney %s",
+    #                 rep(c("healthy", "diseased"), each=2),
+    #                 rep(c("counts", "colData"), 2)),
+    # Description = sprintf("%s for %s in the Wu mouse kidney single-nucleus RNA-seq dataset",
+    #                       rep(c("Count matrix", "Per-cell metadata"), 2),
+    #                       rep(c("healthy tissue", "fibrotic tissue"), each=2)),
+    RDataPath = file.path(
+      "MethylSeqData", "blueprint_blood",
+      # paste0(c("counts-healthy", "coldata-healthy", "counts-disease", "coldata-disease"),
+      # ".rds"))
+    ),
+    BiocVersion = "3.12",
+    Genome = "hg38",
+    SourceType = "TSV",
+    # SourceUrl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE96612",
+    # SourceVersion = c(
+    #   "GSE119531_Healthy.combined.dge.txt.gz",
+    #   "GSE119531_Healthy.combined.cell.annotation.txt.gz",
+    #   "GSE119531_UUO.dge.txt.gz",
+    #   "GSE119531_UUO.cell.annotation.txt.gz"
+    # ),
+    Species = "Homo sapiens",
+    # TaxonomyId = "10090",
+    # Coordinate_1_based = NA,
+    # DataProvider = "GEO",
+    Maintainer = "Peter Hickey <peter.hickey@gmail.com>",
+    # RDataClass = c("dgCMatrix", "data.frame", "dgCMatrix", "data.frame"),
+    # DispatchClass = "Rds"),
+  ),
+  row.names = FALSE)
