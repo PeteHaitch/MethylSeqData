5800a6eb15bbb9697193fe78610877de4b7908c7
Peter Hickey <peter.hickey@gmail.com>
2020-10-01 10:24:52 +1000

    Update ChenMammaryData documentaiton

--- R/ChenMammaryData.R
+++ R/ChenMammaryData.R
@@ -1,18 +1,25 @@
 #' Obtain the Chen mammary data
 #'
 #' Obtain the mouse mammary reduced representation bisulfite sequencing (RRBS) data from Chen et al. (2017).
-#' The data consists of RRBS methylation profiles of epithelial populations from the mouse mammary gland, with \eqn{n = 2} biological replicates for each of three cell populations.
 #'
 #' @param HDF5Array Logical scalar indicating whether the assays should be returned as \linkS4class{HDF5Array} objects or as ordinary \link{matrix} objects.
 #'
 #' @details
-#' Column metadata include the original sample description, population, and sample name.
+#' The data consists of RRBS methylation profiles of epithelial populations from the mouse mammary gland, with \eqn{n = 2} biological replicates for each of three cell populations.
+#' The dataset contains unstranded CpG loci and the counts have not been aggregated by strand.
 #'
+#' Column metadata include:
+#' \itemize{
+#'   \item \code{Sample} The sample ID
+#'   \item \code{Population} The epithelial population ID
+#'   \item \code{Description} The description of the epithelial population
+#' }
+
 #' If \code{HDF5Array = TRUE} then the each assay is returned as a \linkS4class{HDF5Array} object.
-#' Each \linkS4class{HDF5Array} occupies approximately 2 Kb in memory.
+#' Each \linkS4class{HDF5Array} occupies approximately 2 Kib in memory.
 
 #' If \code{HDF5Array = FALSE} then the each assay is returned as an ordinary \link{matrix} objects.
-#' Each matrix occupies approximately 81 Mb in memory.
+#' Each matrix occupies approximately 81 MiB in memory.
 #'
 #' All data are downloaded from ExperimentHub and cached for local re-use.
 #' Specific resources can be retrieved by searching for \code{MethylSeqdata/chen_mammary}.
@@ -22,7 +29,7 @@
 #' @author Peter Hickey
 #'
 #' @references
-#' Chen H et al. (2017).
+#' Chen, et al. (2017).
 #' Differential methylation analysis of reduced representation bisulfite sequencing experiments using edgeR.
 #' \emph{F1000Research} 6, 23-32.
 #' \url{https://doi.org/10.12688/f1000research.13196.2}
